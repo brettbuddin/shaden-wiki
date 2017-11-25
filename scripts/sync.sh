b807ae66645034c6910e7cc1103d4833eb664722
Brett Buddin <brett@buddin.us>
2017-11-25 10:24:17 -0500

    Environment variable naming tweaks.

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -15,21 +15,21 @@ if [ -z "$GITHUB_EMAIL" ]; then
     exit 1
 fi
 
-if [ -z "$GITHUB_WIKI_UPSTREAM" ]; then
-    >&2 echo "Please set GITHUB_WIKI_UPSTREAM."
+if [ -z "$GITHUB_WIKI_UPSTREAM_URL" ]; then
+    >&2 echo "Please set GITHUB_WIKI_UPSTREAM_URL."
     exit 1
 fi
 
-if [ -z "$GITHUB_WIKI_ORIGIN" ]; then
-    >&2 echo "Please set GITHUB_WIKI_ORIGIN."
+if [ -z "$GITHUB_WIKI_ORIGIN_URL" ]; then
+    >&2 echo "Please set GITHUB_WIKI_ORIGIN_URL."
     exit 1
 fi
 
 git config user.name $GITHUB_USERNAME
 git config user.email $GITHUB_EMAIL
 git remote remove origin
-git remote add origin https://$GITHUB_API_KEY@$GITHUB_WIKI_ORIGIN.git
-git remote add upstream https://$GITHUB_API_KEY@$GITHUB_WIKI_UPSTREAM.git
+git remote add origin https://$GITHUB_API_KEY@$GITHUB_WIKI_ORIGIN_URL.git
+git remote add upstream https://$GITHUB_API_KEY@$GITHUB_WIKI_UPSTREAM_URL.git
 git fetch origin
 git fetch upstream
 git merge upstream/master --no-edit
