48e2a5361cb2115e3ba50c4580786fccf7fc3952
Brett Buddin <brett@buddin.us>
2017-11-25 09:29:56 -0500

    Add Travis CI to sync to shaden.wiki

---
+++ scripts/sync.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -e
+
+if [ -z "$GITHUB_API_KEY" ]; then
+    >&2 echo "Please set GITHUB_API_KEY."
+    exit 1
+fi
+
+if [ -z "$GITHUB_USERNAME" ]; then
+    >&2 echo "Please set GITHUB_USERNAME."
+    exit 1
+fi
+
+if [ -z "$GITHUB_EMAIL" ]; then
+    >&2 echo "Please set GITHUB_EMAIL."
+    exit 1
+fi
+
+if [ -z "$GITHUB_WIKI_UPSTREAM" ]; then
+    >&2 echo "Please set GITHUB_WIKI_UPSTREAM."
+    exit 1
+fi
+
+if [ -z "$GITHUB_WIKI_ORIGIN" ]; then
+    >&2 echo "Please set GITHUB_WIKI_ORIGIN."
+    exit 1
+fi
+
+git config user.name $GITHUB_USERNAME
+git config user.email $GITHUB_EMAIL
+git remote remove origin
+git remote add origin https://$GITHUB_API_KEY@$GITHUB_WIKI_ORIGIN.git
+git remote add upstream https://$GITHUB_API_KEY@$GITHUB_WIKI_UPSTREAM.git
+git fetch origin
+git fetch upstream
+git merge upstream/master --no-edit
+git push origin HEAD:master
+git push upstream HEAD:master
